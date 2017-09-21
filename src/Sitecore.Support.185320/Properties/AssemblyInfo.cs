df3237429fd176fcf19196fcab1f13eaf7b3b1f0
DK\pk <pk@sitecore.net>
2017-09-21 16:22:27 +0300

    Initial commit

---
+++ src/Sitecore.Support.185320/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.185320")]
+[assembly: AssemblyProduct("Sitecore.Support.185320")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.185320/SitecoreADProfileProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using LightLDAP.Utility;
+using Reflection = Sitecore.Reflection.ReflectionUtil;
+
+namespace LightLDAP.Support
+{
+  public class SitecoreADProfileProvider: LightLDAP.SitecoreADProfileProvider
+  {
+    public override void Initialize(string name, NameValueCollection config)
+    {
+      base.Initialize(name, config);
+      if (!ReflectionUtil.GetField<bool>(typeof(LightLDAP.SitecoreADProfileProvider), "isInitialized", this))
+      {
+        ReflectionUtil.SetField(typeof(System.Configuration.Provider.ProviderBase), "_name",this, name);
+      }
+    }
+  }
+}
